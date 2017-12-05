2c7dbf881a2c805613fe9e746dac07d2920fb173
Sean Cahill <seancahill25@gmail.com>
2017-12-05 21:53:13 +0000

    now displays weather and temp, deleted apikey

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-/*global $ apiKey*/
+/*global $ apiKey navigator config*/
+var apiKey = config.apiKey;
 $(document).ready(function() {
-	$.ajax({
-	   url: "http://api.openweathermap.org/data/2.5/weather" ,
-	   data: {
-	   		id: apiKey,
-	   		name: "London",
-	 },
-	 success: function(data) {
-	 	$("#weather").text(data.main.temp)
-	 }
-	  
-	});
-	   });
+  $("#weather").on("click", function() {
+    if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=imperial&APPID="+apiKey, function(data) {
+          var city = data.name;
+          var temp = Math.round(data.main.temp);
+          var weather = data.weather[0].description;
+          var image = "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>";
+          $("#temp").html(city+"<br><br>"+temp+"<br><br>"+weather+image);
+        });
+      });
+    }
+  });
+});
--- key.js
+++
@@ -1,1 +0,0 @@
-var apiKey = "c45b3e0c6cd835daff0a27d635824d4b";
