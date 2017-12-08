6df9bf6ab194fe2a00e33e2732cdc46294f9078f
Sean Cahill <seancahill25@gmail.com>
2017-12-08 22:31:01 +0000

    conversion is know on click instead of a button

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 /*global $ apiKey navigator config*/
 var apiKey = config.apiKey;
-var unit;
-$(document).ready(function() {
-  $("#weather").on("click", function() {
+$(document).ready(function() { 
     if (navigator.geolocation) {
      navigator.geolocation.getCurrentPosition(function(position) {
         var lat = position.coords.latitude;
         var long = position.coords.longitude;
+        var temp;
         $.ajax({
           url: "https://api.openweathermap.org/data/2.5/weather",
           data: {
             lat: lat,
             lon: long,
-            units: "metric",
+            units: "imperial",
             APPID: apiKey,
           },
           success: function display(data) {
-          var city = data.name;
-          var temp = Math.round(data.main.temp);
-          var weather = data.weather[0].description;
-          var image = "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>";
-          $("#info").html(city+"<br><br>"+temp+"C"+"<br><br>"+weather+image);
+                     temp = Math.round(data.main.temp);
+                    $('#temp').text(temp.toFixed(0));
+                    $('#description').text(data.weather[0].main);
+                    $('#city').text(data.name);
+                    document.getElementById('icon').innerHTML = '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
           }
         });
-        function convert(temp) {
-    var x;
-    if (temp == "C") {
-        x = document.getElementById("c").value * (9 / 5) + 32;
-        document.getElementById("f").value = Math.round(x);
-    } else {
-        x = (document.getElementById("f").value -32) * (5 / 9);
-        document.getElementById("c").value = Math.round(x);
-    }
-}
       });
-      
     }
-  });
 });
+function changeTemp() {
+    var temp = document.getElementById('temp').innerHTML;
+    var tempChange = document.getElementById('tempChange').innerHTML;
+    var tempf;
+    var tempc;
+    if (tempChange == "C"){
+      document.getElementById('tempChange').innerHTML = "F";
+      tempf = Math.round(temp * 1.8 + 32);
+      document.getElementById('temp').innerHTML = tempf.toFixed(0);
+    } else {
+      document.getElementById('tempChange').innerHTML = "C";
+      tempc = Math.round(temp - 32) / 1.8;
+      document.getElementById('temp').innerHTML = tempc.toFixed(1);
+    }}
